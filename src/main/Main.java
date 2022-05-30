8fe1b6bb162e1dd60a78341aa958d755a1d2fd32
kimchowon <>
2022-05-30 23:47:11 +0900

    프로젝트 구조 변경

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,95 +2,15 @@ package Practice.InsuranceCompany.Design.src.main;
 
 
 
-import Practice.InsuranceCompany.Design.src.contract.ContractList;
-import Practice.InsuranceCompany.Design.src.contract.ContractListImpl;
-import Practice.InsuranceCompany.Design.src.customer.CustomerList;
-import Practice.InsuranceCompany.Design.src.customer.CustomerListImpl;
-import Practice.InsuranceCompany.Design.src.payment.PaymentFormList;
-import Practice.InsuranceCompany.Design.src.payment.PaymentFormListImpl;
-import Practice.InsuranceCompany.Design.src.view.VPayment;
-
-import java.util.Scanner;
+import Practice.InsuranceCompany.Design.src.view.VMain;
 
 public class Main {
-    static Scanner scn;
-    static CustomerList customerList = new CustomerListImpl();
-    static ContractList contractList = new ContractListImpl();
-
-    static PaymentFormList paymentFormList = new PaymentFormListImpl();
-
     public static void main(String[] args) {
-
-        scn = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("----------------------------보험 시스템-----------------------------");
-            System.out.println("1. 상품 관리 2. 계약 관리 3. 지급 관리 4. 사고처리 관리 5. 수금 관리 0. 종료");
-
-            String input = scn.next();
-            System.out.println(input + " 선택");
-
-            if (input.equals("1")) {
-                while(true){
-                    System.out.println("----------------------상품 관리-----------------------");
-                    System.out.println("(1). 상품 조회 (2). 상품 등록 (3). 상품 수정 (4). 상품 삭제");
-                    String input1 = scn.next();
-
-//                    System.out.println(input1 + " 선택");
-                }
-            } else if (input.equals("2")) {
-
-            } else if (input.equals("3")) {
-                while (true) {
-                    VPayment vPayment = new VPayment(scn, customerList, contractList, paymentFormList);
-                    System.out.println("---------------------지급 관리-----------------------");
-                    System.out.println("(1). 지급금 접수받기 (2). 지급금 지급하기 (3). 지급 안내서 전송하기 (4) 뒤로 가기");
-                    int selectPaymentMenu = scn.nextInt();
-
-                    switch (selectPaymentMenu) {
-
-                        case 1:
-                            boolean registerPaymentResult = vPayment.registerPayment();
-
-                            if (registerPaymentResult) System.out.println("제지급금 접수에 성공하였습니다.");
-                            else System.out.println("제지급금 접수에 실패하였습니다.");
-                            return;
-
-                        case 2:
-                            int sendPaymentResult = vPayment.sendPayment();
-
-                            if (sendPaymentResult == -1) System.out.println("제지급금 지급에 실패하였습니다.");
-                            else System.out.println(sendPaymentResult + "원의 제지급금이 지급되었습니다.");
-                            return;
-
-                        case 3:
-                            boolean sendPaymentGuideResult = vPayment.sendPaymentGuide();
-                            if (sendPaymentGuideResult) System.out.println("지급 안내서가 전송 되었습니다.");
-                            else System.out.println("지급 안내서 전송에 실패하였습니다.");
-                            return;
-
-                        case 4:
-                            return;
-
-                        default:
-                            System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
-                            return;
-                    }
-                } // 제지급금 끝
-            } else if (input.equals("4")) {
-
-            } else if (input.equals("5")) {
-                return;
-            } else if (input.equals("0")) {
-                return;
-            } else
-                System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
-
-        }
-
-
+        VMain vMain = new VMain();
+        vMain.run();
     }
-
-
-
 }
+
+/*
+*
+ */
--- src/view/VMain.java
+++ src/view/VMain.java
@@ -1,8 +1,73 @@
 package Practice.InsuranceCompany.Design.src.view;
 
-public class VMain {
-    public void run(){
+import Practice.InsuranceCompany.Design.src.contract.ContractList;
+import Practice.InsuranceCompany.Design.src.contract.ContractListImpl;
+import Practice.InsuranceCompany.Design.src.customer.CustomerList;
+import Practice.InsuranceCompany.Design.src.customer.CustomerListImpl;
+import Practice.InsuranceCompany.Design.src.payment.PaymentFormList;
+import Practice.InsuranceCompany.Design.src.payment.PaymentFormListImpl;
+import Practice.InsuranceCompany.Design.src.subscription.SubscriptionListImpl;
 
+import java.util.Scanner;
+
+public class VMain {
+    CustomerList customerList = new CustomerListImpl();
+    ContractList contractList = new ContractListImpl();
+
+    PaymentFormList paymentFormList = new PaymentFormListImpl();
+
+    private VInsurance vInsurance;
+    private VContract vContract;
+    private VUnderwriting vUnderwriting;
+    private VPayment vPayment;
+    private VAccident vAccident;
+    private VCollectionMoney vCollectionMoney;
+
+
+
+    public void run(){
+        Scanner scn = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("----------------------------보험 시스템-----------------------------");
+            System.out.println("1. 상품 관리 2. 계약 관리 3. Underwriting 4. 지급 관리 5. 사고처리 관리 6. 수금 관리 0. 종료");
+
+
+            String input = scn.next();
+            System.out.println(input + " 선택");
+
+            if (input.equals("1")) {
+                this.vInsurance = new VInsurance();
+                this.vInsurance.run();  //
+//                while(true){
+//                    System.out.println("----------------------상품 관리-----------------------");
+//                    System.out.println("(1). 상품 조회 (2). 상품 등록 (3). 상품 수정 (4). 상품 삭제");
+//                    String input1 = scn.next();
+//
+////                    System.out.println(input1 + " 선택");
+//                }
+            } else if (input.equals("2")) {
+                    this.vContract = new VContract(scn, contractList, ); //
+                    this.vContract.run();
+
+            } else if (input.equals("3")) {
+                    this.vUnderwriting = new VUnderwriting();
+                    this.vUnderwriting.run();
+
+            } else if (input.equals("4")) {
+                this.vPayment = new VPayment(scn, customerList, contractList, paymentFormList);
+                this.vPayment.run();
+
+            } else if (input.equals("5")) {
+                this.vAccident = new VAccident();   //
+                this.vAccident.run();   //
+
+            } else if (input.equals("0")) {
+                return;
+            } else
+                System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+
+        }
     }
 
     public static boolean makeError(){
